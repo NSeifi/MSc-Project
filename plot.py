217ceca7c14b10fb28f507cda1d827d0b01d97c8
NSeifi <53353909+NSeifi@users.noreply.github.com>
2023-02-05 14:40:47 -0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.dates import DateFormatter
 
 dtype = {'SHRCD': float, 'EXCHCD': float, 'SICCD': float, 'ISSUNO': float, 'HEXCD': float, 'HSICCD': float,
          'DLAMT': float, 'DLSTCD': float, 'SHRFLG': float, 'HSICMG': float, 'HSICIG': float, 'DISTCD': float,
@@ -11,8 +12,8 @@ dtype = {'SHRCD': float, 'EXCHCD': float, 'SICCD': float, 'ISSUNO': float, 'HEXC
          'RETX': float, 'vwretd': float, 'vwretx': float, 'ewretd': float, 'ewretx': float, 'sprtrn': float,
          'Mkt-RF': float, 'SMB': float, 'HML': float, 'RF': float, 'IVOL': float}
 
-#filename = "preprocess/company_preprocessed_csv/{}.csv"
-filename = "preprocess/lagged_company_preprocessed_csv/{}.csv"
+filename = "preprocess/company_preprocessed_csv/{}.csv"
+#filename = "preprocess/lagged_company_preprocessed_csv/{}.csv"
 
 ticker = ['MSFT', 'SIRI', 'OKTA', 'GOOG', 'ALGN', 'REGN', 'CHTR', 'NVDA', 'NFLX', 'ILMN', 'IDXX', 'NTES', 'VRTX']
 for ticker in ticker:
@@ -25,7 +26,7 @@ for ticker in ticker:
     x1 = np.zeros(1)
     x2 = np.zeros(1)
     y = np.zeros(1)
-    for year_index, year in enumerate([2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]):
+    for year_index, year in enumerate([2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017,2018, 2019]):
         ret = traindata[traindata['year'] == year].groupby(['year', 'month']).mean()
         x1 = np.append(x1, ret.to_numpy()[:, 0].tolist())
         x2 = np.append(x2, ret.to_numpy()[:, 1].tolist())
@@ -42,6 +43,9 @@ for ticker in ticker:
     plt.plot(y, [t * 10 for t in x1], 'r-', y, x2, 'b-')
     plt.ylabel('')
     plt.grid(axis='x', color='0.95')
+    ticks_data = pd.to_datetime(sorted(set(y.year.tolist())), format='%Y')
+    plt.xticks(ticks=ticks_data, labels=ticks_data)
+    plt.gca().xaxis.set_major_formatter(DateFormatter('%Y'))
     plt.xlabel("Year")
     plt.ylabel("IVOL and (Return * 10)")
     plt.gca().legend(["return", "IVol"])
